eb9bf5714f4d7cb6bc7c88313550aa68398a3171
Seba Lagomarsino <lagomarsinosebastian@gmail.com>
2020-09-18 09:46:16 -0300

    Added .vscode

--- app/App.js
+++ app/App.js
@@ -33,9 +33,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
-  },
 });
