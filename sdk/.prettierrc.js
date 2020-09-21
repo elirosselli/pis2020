c6d998a3108744110e6c1e3dd19fee3fa90ef9c5
Seba Lagomarsino <lagomarsinosebastian@gmail.com>
2020-09-21 20:26:33 -0300

    Changed prettier endOfLine to LF

--- sdk/.prettierrc.js
+++ sdk/.prettierrc.js
@@ -6,7 +6,11 @@ module.exports = {
   tabWidth: 2,
   arrowParens: 'avoid',
 <<<<<<< HEAD
+<<<<<<< HEAD
   endOfLine: 'lf',
 =======
 >>>>>>> aaa2e923... Added react react-native and prop-types dependencies
+=======
+  endOfLine: 'lf',
+>>>>>>> 81fe491b... Changed prettier endOfLine to LF
 };
