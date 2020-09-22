ab1fc20436cae2e2d7a6940d2ec9096ad3f2df29
Santiago Acevedo <santiagoacevedoc@gmail.com>
2020-09-21 21:15:31 -0300

    Added npm locally to avoid publish process

--- sdk/.prettierrc.js
+++ sdk/.prettierrc.js
@@ -5,12 +5,5 @@ module.exports = {
   trailingComma: 'all',
   tabWidth: 2,
   arrowParens: 'avoid',
-<<<<<<< HEAD
-<<<<<<< HEAD
   endOfLine: 'lf',
-=======
->>>>>>> aaa2e923... Added react react-native and prop-types dependencies
-=======
-  endOfLine: 'lf',
->>>>>>> 81fe491b... Changed prettier endOfLine to LF
 };
--- sdk/LoginButton.js
+++ sdk/LoginButton.js
@@ -5,33 +5,9 @@ import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 import styles from './styles';
 import LogoAgesicSimple from './images/logoAgesicSimple.png';
-<<<<<<< HEAD
 import { login } from './requests';
 
 const LoginButton = ({ sdkIdUClientId }) => {
-=======
-import { login } from './requests/requests';
-
-const LoginButton = ({ sdkIdUClientId }) => {
-  // Used for debug
-  const handleOpenURL = event => {
-    console.log(event.url);
-    const route = event.url.replace(/.*?:\/\//g, '');
-    console.log(route);
-    const code = route.match(/\/[^/]+\/?code=([^&]*)/)[1];
-    console.log(`Code: ${code}`);
-    const state = route.match(/\/[^/]+\/?state=([^&]*)/)[1];
-    console.log(`State: ${state}`);
-  };
-
-  useEffect(() => {
-    Linking.addEventListener('url', handleOpenURL);
-    return () => {
-      Linking.removeEventListener('url', handleOpenURL);
-    };
-  }, [handleOpenURL]);
-
->>>>>>> bf8e1072... Import only login funciton instead of all sdk functions to LoginButton
   const handleLogin = () => login(sdkIdUClientId);
 
   return (
