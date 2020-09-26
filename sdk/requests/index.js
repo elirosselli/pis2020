95be81cb4963d482dcecc95c9e86e9e4696407b5
elirosselli <elianarosselli@gmail.com>
2020-09-26 20:00:58 -0300

    Intento fallido de login que devuelve auth code

--- sdk/requests/index.js
+++ sdk/requests/index.js
@@ -2,6 +2,13 @@
 import { Linking } from 'react-native';
 
 export function login(sdkIdUClientId) {
+  const handleOpenUrl = url => {
+    console.log('url: ', url);
+  };
+
+  console.log('agregando event listener...');
+  Linking.addEventListener('url', handleOpenUrl);
+
   return Linking.openURL(
     `https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize?scope=openid&response_type=code&client_id=${sdkIdUClientId}&redirect_uri=sdkIdU.testing%3A%2F%2Fauth`,
   );
