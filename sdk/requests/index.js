7a8501562a25fedcb1ad8b702c4110e695916c2e
Seba Lagomarsino <lagomarsinosebastian@gmail.com>
2020-09-28 21:34:00 -0300

    Added url event listener in login function

--- sdk/requests/index.js
+++ sdk/requests/index.js
@@ -2,14 +2,21 @@
 import { Linking } from 'react-native';
 
 export function login(sdkIdUClientId) {
+  let resolve;
+  // eslint-disable-next-line no-unused-vars
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+  });
+
   const handleOpenUrl = url => {
-    console.log('url: ', url);
+    resolve(url.url);
   };
 
-  console.log('agregando event listener...');
   Linking.addEventListener('url', handleOpenUrl);
 
-  return Linking.openURL(
+  Linking.openURL(
     `https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize?scope=openid&response_type=code&client_id=${sdkIdUClientId}&redirect_uri=sdkIdU.testing%3A%2F%2Fauth`,
   );
+
+  return promise;
 }
