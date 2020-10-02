5288b2839ffadcfd218bd6b51146258a40589f09
Santiago Acevedo <santiagoacevedoc@gmail.com>
2020-10-02 20:53:06 -0300

    Archivo de endpoints

--- sdk/requests/index.js
+++ sdk/requests/index.js
@@ -1,19 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 import { Linking } from 'react-native';
+import { loginEndpoint } from './endpoints';
 
 export const REQUEST_TYPES = {
   LOGIN: 'login',
 };
 
-const makeRequest = (type, sdkIdUClientId) => {
+const makeRequest = (type, clientId) => {
   switch (type) {
     case REQUEST_TYPES.LOGIN: {
-      return (
-        sdkIdUClientId &&
-        Linking.openURL(
-          `https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize?scope=openid&response_type=code&client_id=${sdkIdUClientId}&redirect_uri=sdkIdU.testing%3A%2F%2Fauth`,
-        )
-      );
+      return clientId && Linking.openURL(loginEndpoint(clientId));
     }
     default:
       return 'default value';
