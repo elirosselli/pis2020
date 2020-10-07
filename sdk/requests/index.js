f5f851d0a95b60075f342141e210b017cf3efe3e
Facundo Ezequiel Gutierrez Uran <facundo.gutierrez@fing.edu.uy>
2020-10-07 20:34:54 -0300

    Get token function

--- sdk/requests/index.js
+++ sdk/requests/index.js
@@ -2,14 +2,15 @@
 import { Linking } from 'react-native';
 import { loginEndpoint } from './endpoints';
 import { getParameters } from '../configuration';
+import { encode as btoa } from 'base-64';
+import RNFetchBlob from 'rn-fetch-blob';
 
 export const REQUEST_TYPES = {
   LOGIN: 'login',
   GET_TOKEN: 'getToken',
 };
 
-const makeRequest = type => {
-  const parameters = getParameters();
+const makeRequest = (type, clientId, clientSecret, authCode)=> {
   switch (type) {
     case REQUEST_TYPES.LOGIN: {
       // si hay un clientId setteado, se abre el browser
@@ -20,7 +21,29 @@ const makeRequest = type => {
       );
     }
     case REQUEST_TYPES.GET_TOKEN: {
-      return 'get token functionality';
+      const encodedCredentials = btoa(`${clientId}:${clientSecret}`);
+
+      return RNFetchBlob.config({ trusty: true })
+        .fetch(
+          'POST',
+          tokenEndpoint,
+          {
+            Authorization: `Basic ${encodedCredentials}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            Accept: 'application/json',
+          },
+          `grant_type=authorization_code&code=${authCode}&redirect_uri=sdkIdU.testing%3A%2F%2Fauth`,
+        )
+        .then(resp => {
+          return [resp.json(), resp.respInfo.status];
+        })
+        .then(data => {
+          if (data[1] === 400) {
+            return Promise.reject(data[0]);
+          } else {
+            return Promise.resolve(data[0]);
+          }
+        });
     }
     default:
       return 'default value';
