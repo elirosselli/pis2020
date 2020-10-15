bae4496ae11c3540065c43472fbb533e330c59d0
Belen Brandino <mbelub@gmail.com>
2020-10-15 15:09:01 -0300

    Fixed linter issues

--- sdk/requests/__tests__/index.test.js
+++ sdk/requests/__tests__/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-identical-functions */
 import { fetch } from 'react-native-ssl-pinning';
 import makeRequest, { REQUEST_TYPES } from '../index';
 import login from '../login';
