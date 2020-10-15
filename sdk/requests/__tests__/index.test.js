5e1361fb0d776a9fb0b02393e96f963514e8dab4
Belen Brandino <mbelub@gmail.com>
2020-10-15 00:25:54 -0300

    First refreshToken test

--- sdk/requests/__tests__/index.test.js
+++ sdk/requests/__tests__/index.test.js
@@ -119,6 +119,64 @@ describe('getToken', () => {
   });
 });
 
+
+describe('makeRequest refreshToken', () => {
+  it('calls refreshToken with correct code', async () => {
+    // Mockear la funcion fetch
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () =>
+          Promise.resolve({
+            access_token: 'c9747e3173544b7b870d48aeafa0f661',
+            expires_in: 3600,
+            id_token:
+              'eyJhbGciOiJSUzI1NiIsImtpZCI6IjdhYThlN2YzOTE2ZGNiM2YyYTUxMWQzY2ZiMTk4YmY0In0.eyJpc3MiOiJodHRwczovL2F1dGgtdGVzdGluZy5pZHVydWd1YXkuZ3ViLnV5L29pZGMvdjEiLCJzdWIiOiI1ODU5IiwiYXVkIjoiODk0MzI5IiwiZXhwIjoxNjAxNTA2Nzc5LCJpYXQiOjE2MDE1MDYxNzksImF1dGhfdGltZSI6MTYwMTUwMTA0OSwiYW1yIjpbInVybjppZHVydWd1YXk6YW06cGFzc3dvcmQiXSwiYWNyIjoidXJuOmlkdXJ1Z3VheTpuaWQ6MSIsImF0X2hhc2giOiJmZ1pFMG1DYml2ZmxBcV95NWRTT09RIn0.r2kRakfFjIXBSWlvAqY-hh9A5Em4n5SWIn9Dr0IkVvnikoAh_E1OPg1o0IT1RW-0qIt0rfkoPUDCCPNrl6d_uNwabsDV0r2LgBSAhjFIQigM37H1buCAn6A5kiUNh8h_zxKxwA8qqia7tql9PUYwNkgslAjgCKR79imMz4j53iw',
+            refresh_token: '041a156232ac43c6b719c57b7217c9ee',
+            token_type: 'bearer',
+          }),
+      }),
+    );
+
+    const clientId = '898562';
+    const clientSecret =
+      'cdc04f19ac2s2f5h8f6we6d42b37e85a63f1w2e5f6sd8a4484b6b94b';
+    const tokenEndpoint = 'https://auth-testing.iduruguay.gub.uy/oidc/v1/token';
+    const refreshToken = '541a156232ac43c6b719c57b7217c9ea';
+
+    // Mockear getParameters
+    getParameters.mockReturnValue({
+      clientId,
+      clientSecret,
+      refreshToken,
+    });
+
+    const encodedCredentials =
+      'ODk4NTYyOmNkYzA0ZjE5YWMyczJmNWg4ZjZ3ZTZkNDJiMzdlODVhNjNmMXcyZTVmNnNkOGE0NDg0YjZiOTRi';
+
+    const response = await makeRequest(REQUEST_TYPES.GET_REFRESH_TOKEN);
+
+    expect.assertions(2);
+
+    // Chequeo de parametros enviados
+    expect(fetch).toHaveBeenCalledWith(tokenEndpoint, {
+      method: 'POST',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+      headers: {
+        Authorization: `Basic ${encodedCredentials}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        Accept: 'application/json',
+      },
+      body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+    });
+
+    // Chequeo de respuestas
+    expect(response).toBe('c9747e3173544b7b870d48aeafa0f661');
+  });
+}); 
+
 describe('default', () => {
   it('calls default', async () => {
     const response = await makeRequest('default');
