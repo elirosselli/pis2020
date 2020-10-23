24c39750fbe9552f96e532f814044ccb178b9429
manueleirea <manueleirea98@gmail.com>
2020-10-23 17:17:16 -0300

    Uncommented some code

--- sdk/__tests__/configuration-requests.test.js
+++ sdk/__tests__/configuration-requests.test.js
@@ -34,48 +34,48 @@ beforeEach(() => {
   });
 });
 
-// describe('configuration module and make request type login integration', () => {
-//   it('calls initialize and makes a login request', async () => {
-//     const correctLoginEndpoint =
-//       'https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize?scope=openid&response_type=code&client_id=clientId&redirect_uri=redirectUri';
-//     const fetchRedirectUri = 'redirectUri';
-//     const fetchClientId = 'clientId';
-//     const fetchClientSecret = 'clientSecret';
-//     initialize(fetchRedirectUri, fetchClientId, fetchClientSecret);
+describe('configuration module and make request type login integration', () => {
+  it('calls initialize and makes a login request', async () => {
+    const correctLoginEndpoint =
+      'https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize?scope=openid&response_type=code&client_id=clientId&redirect_uri=redirectUri';
+    const fetchRedirectUri = 'redirectUri';
+    const fetchClientId = 'clientId';
+    const fetchClientSecret = 'clientSecret';
+    initialize(fetchRedirectUri, fetchClientId, fetchClientSecret);
 
-//     let parameters = getParameters();
-//     expect(parameters.clientId).toBe(fetchClientId);
-//     expect(parameters.clientSecret).toBe(fetchClientSecret);
-//     expect(parameters.redirectUri).toBe(fetchRedirectUri);
-//     expect(parameters.code).toBe('');
-//     expect(parameters.accessToken).toBe('');
-//     expect(parameters.refreshToken).toBe('');
-//     expect(parameters.tokenType).toBe('');
-//     expect(parameters.expiresIn).toBe('');
-//     expect(parameters.idToken).toBe('');
+    let parameters = getParameters();
+    expect(parameters.clientId).toBe(fetchClientId);
+    expect(parameters.clientSecret).toBe(fetchClientSecret);
+    expect(parameters.redirectUri).toBe(fetchRedirectUri);
+    expect(parameters.code).toBe('');
+    expect(parameters.accessToken).toBe('');
+    expect(parameters.refreshToken).toBe('');
+    expect(parameters.tokenType).toBe('');
+    expect(parameters.expiresIn).toBe('');
+    expect(parameters.idToken).toBe('');
 
-//     mockAddEventListener.mockImplementation((eventType, eventHandler) => {
-//       if (eventType === 'url')
-//         eventHandler({
-//           url: `${parameters.redirectUri}?code=35773ab93b5b4658b81061ce3969efc2&state=TEST_STATE`,
-//         });
-//     });
-//     const code = await makeRequest(REQUEST_TYPES.LOGIN);
-//     expect(mockLinkingOpenUrl).toHaveBeenCalledTimes(1);
-//     expect(mockLinkingOpenUrl).toHaveBeenCalledWith(correctLoginEndpoint);
-//     expect(code).toBe('35773ab93b5b4658b81061ce3969efc2');
-//     parameters = getParameters();
-//     expect(parameters.clientId).toBe(fetchClientId);
-//     expect(parameters.clientSecret).toBe(fetchClientSecret);
-//     expect(parameters.redirectUri).toBe(fetchRedirectUri);
-//     expect(parameters.code).toBe('35773ab93b5b4658b81061ce3969efc2');
-//     expect(parameters.accessToken).toBe('');
-//     expect(parameters.refreshToken).toBe('');
-//     expect(parameters.tokenType).toBe('');
-//     expect(parameters.expiresIn).toBe('');
-//     expect(parameters.idToken).toBe('');
-//   });
-// });
+    mockAddEventListener.mockImplementation((eventType, eventHandler) => {
+      if (eventType === 'url')
+        eventHandler({
+          url: `${parameters.redirectUri}?code=35773ab93b5b4658b81061ce3969efc2&state=TEST_STATE`,
+        });
+    });
+    const code = await makeRequest(REQUEST_TYPES.LOGIN);
+    expect(mockLinkingOpenUrl).toHaveBeenCalledTimes(1);
+    expect(mockLinkingOpenUrl).toHaveBeenCalledWith(correctLoginEndpoint);
+    expect(code).toBe('35773ab93b5b4658b81061ce3969efc2');
+    parameters = getParameters();
+    expect(parameters.clientId).toBe(fetchClientId);
+    expect(parameters.clientSecret).toBe(fetchClientSecret);
+    expect(parameters.redirectUri).toBe(fetchRedirectUri);
+    expect(parameters.code).toBe('35773ab93b5b4658b81061ce3969efc2');
+    expect(parameters.accessToken).toBe('');
+    expect(parameters.refreshToken).toBe('');
+    expect(parameters.tokenType).toBe('');
+    expect(parameters.expiresIn).toBe('');
+    expect(parameters.idToken).toBe('');
+  });
+});
 
 // For get token and refresh token
 const expectedIdToken =
@@ -155,75 +155,75 @@ describe('configuration module and make request type get token integration', () 
   });
 });
 
-// describe('configuration module and make request type refresh token integration', () => {
-//   it('calls setParameters and makes a refresh token request ', async () => {
-//     const fetchClientId = '898562';
-//     const fetchClientSecret =
-//       'cdc04f19ac2s2f5h8f6we6d42b37e85a63f1w2e5f6sd8a4484b6b94b';
-//     const fetchRefreshToken = '041a156232ac43c6b719c57b7217c9ee';
-//     const returnedAccessToken = 'c9747e3173544b7b870d48aeafa0f661';
-//     const returnedRefreshToken = '041a156232ac43c6b719c57b7217c9ea';
+describe('configuration module and make request type refresh token integration', () => {
+  it('calls setParameters and makes a refresh token request ', async () => {
+    const fetchClientId = '898562';
+    const fetchClientSecret =
+      'cdc04f19ac2s2f5h8f6we6d42b37e85a63f1w2e5f6sd8a4484b6b94b';
+    const fetchRefreshToken = '041a156232ac43c6b719c57b7217c9ee';
+    const returnedAccessToken = 'c9747e3173544b7b870d48aeafa0f661';
+    const returnedRefreshToken = '041a156232ac43c6b719c57b7217c9ea';
 
-//     setParameters({
-//       clientId: fetchClientId,
-//       clientSecret: fetchClientSecret,
-//       refreshToken: fetchRefreshToken,
-//     });
-//     let parameters = getParameters();
-//     expect(parameters.clientId).toBe(fetchClientId);
-//     expect(parameters.clientSecret).toBe(fetchClientSecret);
-//     expect(parameters.redirectUri).toBe('');
-//     expect(parameters.code).toBe('');
-//     expect(parameters.accessToken).toBe('');
-//     expect(parameters.refreshToken).toBe(fetchRefreshToken);
-//     expect(parameters.tokenType).toBe('');
-//     expect(parameters.expiresIn).toBe('');
-//     expect(parameters.idToken).toBe('');
+    setParameters({
+      clientId: fetchClientId,
+      clientSecret: fetchClientSecret,
+      refreshToken: fetchRefreshToken,
+    });
+    let parameters = getParameters();
+    expect(parameters.clientId).toBe(fetchClientId);
+    expect(parameters.clientSecret).toBe(fetchClientSecret);
+    expect(parameters.redirectUri).toBe('');
+    expect(parameters.code).toBe('');
+    expect(parameters.accessToken).toBe('');
+    expect(parameters.refreshToken).toBe(fetchRefreshToken);
+    expect(parameters.tokenType).toBe('');
+    expect(parameters.expiresIn).toBe('');
+    expect(parameters.idToken).toBe('');
 
-//     fetch.mockImplementation(() =>
-//       Promise.resolve({
-//         status: 200,
-//         json: () =>
-//           Promise.resolve({
-//             access_token: returnedAccessToken,
-//             expires_in: 3600,
-//             id_token: expectedIdToken,
-//             refresh_token: returnedRefreshToken,
-//             token_type: 'bearer',
-//           }),
-//       }),
-//     );
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () =>
+          Promise.resolve({
+            access_token: returnedAccessToken,
+            expires_in: 3600,
+            id_token: expectedIdToken,
+            refresh_token: returnedRefreshToken,
+            token_type: 'bearer',
+          }),
+      }),
+    );
 
-//     const encodedCredentials =
-//       'ODk4NTYyOmNkYzA0ZjE5YWMyczJmNWg4ZjZ3ZTZkNDJiMzdlODVhNjNmMXcyZTVmNnNkOGE0NDg0YjZiOTRi';
+    const encodedCredentials =
+      'ODk4NTYyOmNkYzA0ZjE5YWMyczJmNWg4ZjZ3ZTZkNDJiMzdlODVhNjNmMXcyZTVmNnNkOGE0NDg0YjZiOTRi';
 
-//     const response = await makeRequest(REQUEST_TYPES.GET_REFRESH_TOKEN);
-//     expect(fetch).toHaveBeenCalledWith(correctTokenEndpoint, {
-//       method: 'POST',
-//       sslPinning: {
-//         certs: ['certificate'],
-//       },
-//       headers: {
-//         Authorization: `Basic ${encodedCredentials}`,
-//         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-//         Accept: 'application/json',
-//       },
-//       body: `grant_type=refresh_token&refresh_token=${fetchRefreshToken}`, // Se usa el refresh token viejo, ya que la funcion makeRequest cambia el valor de parameters.refreshToken
-//     });
+    const response = await makeRequest(REQUEST_TYPES.GET_REFRESH_TOKEN);
+    expect(fetch).toHaveBeenCalledWith(correctTokenEndpoint, {
+      method: 'POST',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+      headers: {
+        Authorization: `Basic ${encodedCredentials}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        Accept: 'application/json',
+      },
+      body: `grant_type=refresh_token&refresh_token=${fetchRefreshToken}`, // Se usa el refresh token viejo, ya que la funcion makeRequest cambia el valor de parameters.refreshToken
+    });
 
-//     expect(response).toBe(returnedAccessToken);
-//     parameters = getParameters();
-//     expect(parameters.clientId).toBe(fetchClientId);
-//     expect(parameters.clientSecret).toBe(fetchClientSecret);
-//     expect(parameters.redirectUri).toBe('');
-//     expect(parameters.code).toBe('');
-//     expect(parameters.accessToken).toBe(returnedAccessToken);
-//     expect(parameters.refreshToken).toBe(returnedRefreshToken);
-//     expect(parameters.tokenType).toBe('bearer');
-//     expect(parameters.expiresIn).toBe(3600);
-//     expect(parameters.idToken).toBe(expectedIdToken);
-//   });
-// });
+    expect(response).toBe(returnedAccessToken);
+    parameters = getParameters();
+    expect(parameters.clientId).toBe(fetchClientId);
+    expect(parameters.clientSecret).toBe(fetchClientSecret);
+    expect(parameters.redirectUri).toBe('');
+    expect(parameters.code).toBe('');
+    expect(parameters.accessToken).toBe(returnedAccessToken);
+    expect(parameters.refreshToken).toBe(returnedRefreshToken);
+    expect(parameters.tokenType).toBe('bearer');
+    expect(parameters.expiresIn).toBe(3600);
+    expect(parameters.idToken).toBe(expectedIdToken);
+  });
+});
 
 describe('configuration module and make request type logout integration', () => {
   it('calls set parameters with empty postLogoutRedirectUri and makes a logout request which returns error', async () => {
