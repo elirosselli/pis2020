0a8d4fd59c502e9f2c1c9f771aa59dc52d808346
manueleirea <manueleirea98@gmail.com>
2020-11-03 16:05:23 -0300

    Added comments to new logout implementation

--- sdk/requests/logout.js
+++ sdk/requests/logout.js
@@ -5,8 +5,8 @@ import { getParameters, clearParameters } from '../configuration';
 
 const logout = async () => {
   const parameters = getParameters();
-  // const logoutEndpointWithParams = `${logoutEndpoint}?id_token_hint=${parameters.idToken}&post_logout_redirect_uri=${parameters.postLogoutRedirectUri}&state=${parameters.state}`;
   try {
+    // Se arma la solicitud a enviar al logoutEndpoint.
     const response = await fetch(logoutEndpoint(), {
       method: 'GET',
       pkPinning: Platform.OS === 'ios',
@@ -18,6 +18,8 @@ const logout = async () => {
     const urlCheck = await response.url;
     const missingParamsMessage = 'Missing required parameter(s): ';
 
+    // Si los parámetros obligatorios para la request se encuentran
+    // inicializados, se procede a evaluar la respuesta del OP.
     if (parameters.postLogoutRedirectUri && parameters.idToken) {
       if (status === 200) {
         if (urlCheck === logoutEndpoint()) {
@@ -26,10 +28,18 @@ const logout = async () => {
           if (state) return Promise.resolve(state[1]);
           return Promise.resolve();
         }
+        // Si la url contenida en la respuesta no coincide con el
+        // logoutEnpoint, se rechaza la promesa retornando un error.
         return Promise.reject(Error('Invalid returned url'));
       }
+      // En caso de que el estado de la respuesta no sea 200,
+      // se rechaza la promesa retornando un error.
       return Promise.reject(Error('Response status not OK'));
     }
+    // Si alguno de los parámetros obligatorios para la request
+    // no se encuentra inicializado, se rechaza la promesa y se
+    // retorna un error que especifica cuál o cuáles parámetros
+    // faltaron.
     if (parameters.postLogoutRedirectUri)
       return Promise.reject(Error(`${missingParamsMessage}idTokenHint`));
     if (parameters.idToken)
@@ -40,6 +50,8 @@ const logout = async () => {
       Error(`${missingParamsMessage}idTokenHint, postLogoutRedirectUri`),
     );
   } catch (error) {
+    // Si existe algun error, se rechaza la promesa y se
+    // retorna dicho error.
     return Promise.reject(error);
   }
 };
