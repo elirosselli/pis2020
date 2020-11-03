1b42d8fdafa127d8ba34e6f36c8d848c5b499cbd
Facundo Ezequiel Gutierrez Uran <facundo.gutierrez@fing.edu.uy>
2020-11-03 20:41:06 -0300

    Retries

--- sdk/requests/getUserInfo.js
+++ sdk/requests/getUserInfo.js
@@ -1,27 +1,29 @@
-import { fetch } from 'react-native-ssl-pinning';
+// import { fetch } from 'react-native-ssl-pinning';
 import { Platform } from 'react-native';
 import { userInfoEndpoint } from '../utils/endpoints';
 import { getParameters } from '../configuration';
-import { ERRORS } from '../utils/constants';
+import fetchWrapper from '../utils/fetchRetry';
 
 const getUserInfo = async () => {
-  const { accessToken } = getParameters();
-  if (!accessToken) {
-    return Promise.reject(ERRORS.INVALID_TOKEN);
-  }
+  let { accessToken } = getParameters();
   try {
-    const response = await fetch(userInfoEndpoint(), {
-      method: 'GET',
-      pkPinning: Platform.OS === 'ios',
-      sslPinning: {
-        certs: ['certificate'],
+    const response = await fetchWrapper(
+      userInfoEndpoint,
+      {
+        method: 'GET',
+        timeoutInterval: 10000,
+        pkPinning: Platform.OS === 'ios',
+        sslPinning: {
+          certs: ['certificate'],
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          Accept: 'application/json',
+        },
       },
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        Accept: 'application/json',
-      },
-    });
+      5,
+    );
     const { status } = response;
     const responseJson = await response.json();
     // En caso de error se devuelve la respuesta,
---
+++ sdk/utils/fetchRetry.js
@@ -0,0 +1,12 @@
+import { fetch } from 'react-native-ssl-pinning';
+
+const fetchWrapper = (url, options = {}, n) => {
+  const fetchOptions = { ...options };
+  console.log(`fetch ${n}`);
+  return fetch(url, fetchOptions).catch(error => {
+    if (n === 1) throw error;
+    return fetchWrapper(url, fetchOptions, n - 1);
+  });
+};
+
+export default fetchWrapper;
