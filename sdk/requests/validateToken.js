212f0e66e83bd346b7e4c92c5b4285981fd8e83f
facugu1998 <facundo.gutierrez@fing.edu.uy>
2020-11-12 04:54:35 -0300

    Feature/request validate token (#49)
    * Validate Token
    
    * Added library for verification
    
    * Fixed exponent and modulous
    
    * Tests terminados
    
    * tests encoding
    
    * Comment Fixes
    
    * Comment Fixes
    
    * coverage 100%
    
    * remove dependencies
    
    * Moved validateTokenSecurity to a different file
    
    * Fixed linter
    
    * Added support for iOS
    
    * Updated Podfile.lock
    
    * fixes
    
    * Ignored security index file in coverage
    
    * Explain jwk(s)
    
    * reject test
    
    * integration tests
    
    * Fixed include error
    
    * Fixed error types
    
    * Fixes. Validate button
    
    * Add validateToken button
    
    * fixes
    
    * Fixed linter
    
    * Update sdk/security/validateTokenSecurity.js
    
    Co-authored-by: sebalagomarsino <48483705+sebalagomarsino@users.noreply.github.com>
    
    * Update README.md
    
    * Merge with develop and fixes
    
    * Remove enconding functions mock
    
    * Reused endpoints prefix
    
    Co-authored-by: Seba Lagomarsino <lagomarsinosebastian@gmail.com>
    Co-authored-by: Belen Brandino <mbelub@gmail.com>
    Co-authored-by: sebalagomarsino <48483705+sebalagomarsino@users.noreply.github.com>

---
+++ sdk/requests/validateToken.js
@@ -0,0 +1,32 @@
+import { Platform } from 'react-native';
+import { fetch } from 'react-native-ssl-pinning';
+import { validateTokenEndpoint } from '../utils/endpoints';
+import { validateTokenSecurity } from '../security';
+import { ERRORS } from '../utils/constants';
+
+const validateToken = async () => {
+  try {
+    // Obtener la jwk del jwks endpoint.
+    // JSON Web Key (JWK): estandar de representación de una clave criptográfica en formato JSON.
+    // JSON Web Key Set (JWKS): conjunto de JWKs.
+    // JWKS Endpoint: expone las claves y algoritmos que el OP usa. Útil para verificar la autenticidad de los tokens emitidos.
+    const jwksResponse = await fetch(validateTokenEndpoint(), {
+      method: 'GET',
+      pkPinning: Platform.OS === 'ios',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+      headers: {
+        Accept: 'application/json; charset=utf-8',
+      },
+    });
+    // Convertir a formato json.
+    const jwksResponseJson = await jwksResponse.json();
+    // Validar el token en el módulo de seguridad a partir de la jwk.
+    return validateTokenSecurity(jwksResponseJson);
+  } catch (error) {
+    return Promise.reject(ERRORS.FAILED_REQUEST);
+  }
+};
+
+export default validateToken;
--- sdk/utils/endpoints.js
+++ sdk/utils/endpoints.js
@@ -1,30 +1,49 @@
 import { getParameters } from '../configuration';
 
-const productionPrefix = 'https://auth.iduruguay.gub.uy/oidc/v1/';
-const developmentPrefix = 'https://auth-testing.iduruguay.gub.uy/oidc/v1/';
+const productionPrefix = 'https://auth.iduruguay.gub.uy/oidc/v1';
+const developmentPrefix = 'https://auth-testing.iduruguay.gub.uy/oidc/v1';
 
 const tokenEndpoint = () => {
   const { production } = getParameters();
   const endpointPrefix = production ? productionPrefix : developmentPrefix;
-  return `${endpointPrefix}token`;
+  return `${endpointPrefix}/token`;
 };
 
 const loginEndpoint = () => {
   const { production, redirectUri, clientId, scope } = getParameters();
   const endpointPrefix = production ? productionPrefix : developmentPrefix;
-  return `${endpointPrefix}authorize?scope=openid%20${scope}&response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+  return `${endpointPrefix}/authorize?scope=openid%20${scope}&response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
 };
 
 const userInfoEndpoint = () => {
   const { production } = getParameters();
   const endpointPrefix = production ? productionPrefix : developmentPrefix;
-  return `${endpointPrefix}userinfo`;
+  return `${endpointPrefix}/userinfo`;
 };
 
 const logoutEndpoint = () => {
   const { production, idToken, postLogoutRedirectUri, state } = getParameters();
   const endpointPrefix = production ? productionPrefix : developmentPrefix;
-  return `${endpointPrefix}logout?id_token_hint=${idToken}&post_logout_redirect_uri=${postLogoutRedirectUri}&state=${state}`;
+  return `${endpointPrefix}/logout?id_token_hint=${idToken}&post_logout_redirect_uri=${postLogoutRedirectUri}&state=${state}`;
 };
 
-export { loginEndpoint, userInfoEndpoint, tokenEndpoint, logoutEndpoint };
+const validateTokenEndpoint = () => {
+  const { production } = getParameters();
+  const endpointPrefix = production ? productionPrefix : developmentPrefix;
+  return `${endpointPrefix}/jwks`;
+};
+
+const issuer = () => {
+  const { production } = getParameters();
+  const endpointPrefix = production ? productionPrefix : developmentPrefix;
+  return `${endpointPrefix}`;
+};
+
+export {
+  loginEndpoint,
+  userInfoEndpoint,
+  tokenEndpoint,
+  logoutEndpoint,
+  validateTokenEndpoint,
+  issuer,
+};
