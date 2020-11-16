68e6cf5c0d9145c0b3a1353a0bef4b140c413daf
manueleirea <manueleirea98@gmail.com>
2020-11-16 20:22:27 -0300

    Tests to 100% coverage, fixed logout test

--- sdk/requests/logout.js
+++ sdk/requests/logout.js
@@ -37,13 +37,12 @@ const logout = async () => {
       if (urlCheck === logoutEndpoint()) {
         const state = urlCheck.match(/&state=([^&]+)/);
         clearParameters();
-        if (state)
-          return Promise.resolve({
-            message: ERRORS.NO_ERROR,
-            errorCode: ERRORS.NO_ERROR.errorCode,
-            errorDescription: ERRORS.NO_ERROR.errorDescription,
-            state: state[1],
-          });
+        return Promise.resolve({
+          message: ERRORS.NO_ERROR,
+          errorCode: ERRORS.NO_ERROR.errorCode,
+          errorDescription: ERRORS.NO_ERROR.errorDescription,
+          state: state[1],
+        });
       }
       // Si la url contenida en la respuesta no coincide con el
       // logoutEnpoint, se rechaza la promesa retornando un error.
