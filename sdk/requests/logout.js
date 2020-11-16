b352c462962d5e5597e2232cd96d9706e866029f
manueleirea <manueleirea98@gmail.com>
2020-11-16 15:31:25 -0300

    Started fix

--- sdk/requests/logout.js
+++ sdk/requests/logout.js
@@ -44,11 +44,6 @@ const logout = async () => {
             errorDescription: ERRORS.NO_ERROR.errorDescription,
             state: state[1],
           });
-        return Promise.resolve({
-          message: ERRORS.NO_ERROR,
-          errorCode: ERRORS.NO_ERROR.errorCode,
-          errorDescription: ERRORS.NO_ERROR.errorDescription,
-        });
       }
       // Si la url contenida en la respuesta no coincide con el
       // logoutEnpoint, se rechaza la promesa retornando un error.
