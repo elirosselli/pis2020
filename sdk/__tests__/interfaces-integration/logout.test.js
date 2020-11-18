e974230fd0d401eced6a24e70de3772cf9e5587c
Seba Lagomarsino <lagomarsinosebastian@gmail.com>
2020-11-18 17:38:14 -0300

    Add logout security integration tests

--- sdk/__tests__/interfaces-integration/logout.test.js
+++ sdk/__tests__/interfaces-integration/logout.test.js
@@ -36,10 +36,11 @@ beforeEach(() => {
 const idToken =
   'eyJhbGciOiJSUzI1NiIsImtpZCI6IjdhYThlN2YzOTE2ZGNiM2YyYTUxMWQzY2ZiMTk4YmY0In0.eyJpc3MiOiJodHRwczovL2F1dGgtdGVzdGluZy5pZHVydWd1YXkuZ3ViLnV5L29pZGMvdjEiLCJzdWIiOiI1ODU5IiwiYXVkIjoiODk0MzI5IiwiZXhwIjoxNjAxNTA2Nzc5LCJpYXQiOjE2MDE1MDYxNzksImF1dGhfdGltZSI6MTYwMTUwMTA0OSwiYW1yIjpbInVybjppZHVydWd1YXk6YW06cGFzc3dvcmQiXSwiYWNyIjoidXJuOmlkdXJ1Z3VheTpuaWQ6MSIsImF0X2hhc2giOiJmZ1pFMG1DYml2ZmxBcV95NWRTT09RIn0.r2kRakfFjIXBSWlvAqY-hh9A5Em4n5SWIn9Dr0IkVvnikoAh_E1OPg1o0IT1RW-0qIt0rfkoPUDCCPNrl6d_uNwabsDV0r2LgBSAhjFIQigM37H1buCAn6A5kiUNh8h_zxKxwA8qqia7tql9PUYwNkgslAjgCKR79imMz4j53iw';
 const correctLogoutEndpoint = `https://auth-testing.iduruguay.gub.uy/oidc/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=&state=${mockState}`;
+const correctLogoutProductionEndpoint = `https://auth.iduruguay.gub.uy/oidc/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=&state=${mockState}`;
 const invalidUrl = `https://auth-testing.iduruguay.gub.uy/oidc/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=&state=`;
 
-describe('configuration module and logout integration', () => {
-  it('calls set parameters and logout which returns non-empty state', async () => {
+describe('configuration & security modules and logout integration', () => {
+  it('calls set parameters and logout', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
@@ -97,7 +98,194 @@ describe('configuration module and logout integration', () => {
     });
   });
 
-  it('calls set parameters and logout which returns empty state', async () => {
+  it('calls set parameters and logout with production set to true', async () => {
+    const production = true;
+    setParameters({ idToken, production });
+
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken,
+      state: '',
+      scope: '',
+    });
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        url: correctLogoutProductionEndpoint,
+      }),
+    );
+    const response = await logout();
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(correctLogoutProductionEndpoint, {
+      method: 'GET',
+      pkPinning: Platform.OS === 'ios',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+    });
+    expect(response).toStrictEqual({
+      state: mockState,
+      message: ERRORS.NO_ERROR,
+      errorCode: ERRORS.NO_ERROR.errorCode,
+      errorDescription: ERRORS.NO_ERROR.errorDescription,
+    });
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+  });
+
+  it('calls set parameters and logout with empty idTokenHint', async () => {
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+
+    try {
+      await logout();
+    } catch (error) {
+      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+    }
+    expect(fetch).not.toHaveBeenCalled();
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+    expect.assertions(4);
+  });
+
+  it('calls set parameters and logout with invalid idTokenHint', async () => {
+    setParameters({ idToken: 'invalid_id_token' });
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+
+    try {
+      await logout();
+    } catch (error) {
+      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+    }
+    expect(fetch).not.toHaveBeenCalled();
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+    expect.assertions(4);
+  });
+
+  it('calls set parameters and logout with invalid production', async () => {
+    setParameters({ production: 'invalid_production' });
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+
+    try {
+      await logout();
+    } catch (error) {
+      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+    }
+    expect(fetch).not.toHaveBeenCalled();
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+    expect.assertions(4);
+  });
+
+  it('calls set parameters and logout, fetch returns empty state', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
@@ -154,7 +342,9 @@ describe('configuration module and logout integration', () => {
     expect.assertions(5);
   });
 
-  it('calls set parameters with empty idTokenHint and then calls logout which returns error', async () => {
+  it('calls set parameters and logout, fetch returns invalid url', async () => {
+    setParameters({ idToken });
+
     let parameters = getParameters();
     expect(parameters).toStrictEqual({
       redirectUri: '',
@@ -166,18 +356,26 @@ describe('configuration module and logout integration', () => {
       refreshToken: '',
       tokenType: '',
       expiresIn: '',
-      idToken: '',
+      idToken,
       state: '',
       scope: '',
     });
-
+    fetch.mockImplementation(() =>
+      Promise.resolve({ status: 200, url: 'InvalidUrl' }),
+    );
     try {
       await logout();
     } catch (error) {
-      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+      expect(error).toBe(ERRORS.INVALID_URL_LOGOUT);
     }
-    expect(fetch).not.toHaveBeenCalled();
-
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(correctLogoutEndpoint, {
+      method: 'GET',
+      pkPinning: Platform.OS === 'ios',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+    });
     parameters = getParameters();
     expect(parameters).toStrictEqual({
       redirectUri: '',
@@ -189,14 +387,14 @@ describe('configuration module and logout integration', () => {
       refreshToken: '',
       tokenType: '',
       expiresIn: '',
-      idToken: '',
-      state: '',
+      idToken,
+      state: mockState,
       scope: '',
     });
-    expect.assertions(4);
+    expect.assertions(5);
   });
 
-  it('calls set parameters and logout with required parameters, response not OK', async () => {
+  it('calls set parameters and logout, fetch fails (returning 404)', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
@@ -251,59 +449,7 @@ describe('configuration module and logout integration', () => {
     expect.assertions(5);
   });
 
-  it('calls set parameters and logout with required parameters, returns invalid url', async () => {
-    setParameters({ idToken });
-
-    let parameters = getParameters();
-    expect(parameters).toStrictEqual({
-      redirectUri: '',
-      clientId: '',
-      clientSecret: '',
-      production: false,
-      code: '',
-      accessToken: '',
-      refreshToken: '',
-      tokenType: '',
-      expiresIn: '',
-      idToken,
-      state: '',
-      scope: '',
-    });
-    fetch.mockImplementation(() =>
-      Promise.resolve({ status: 200, url: Error('Invalid returned url') }),
-    );
-    try {
-      await logout();
-    } catch (error) {
-      expect(error).toBe(ERRORS.INVALID_URL_LOGOUT);
-    }
-    expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith(correctLogoutEndpoint, {
-      method: 'GET',
-      pkPinning: Platform.OS === 'ios',
-      sslPinning: {
-        certs: ['certificate'],
-      },
-    });
-    parameters = getParameters();
-    expect(parameters).toStrictEqual({
-      redirectUri: '',
-      clientId: '',
-      clientSecret: '',
-      production: false,
-      code: '',
-      accessToken: '',
-      refreshToken: '',
-      tokenType: '',
-      expiresIn: '',
-      idToken,
-      state: mockState,
-      scope: '',
-    });
-    expect.assertions(5);
-  });
-
-  it('calls set parameters and logout with required parameters, fails', async () => {
+  it('calls set parameters and logout, fetch fails (promise rejected)', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
--- sdk/__tests__/requests-integration/logout.test.js
+++ sdk/__tests__/requests-integration/logout.test.js
@@ -37,10 +37,11 @@ beforeEach(() => {
 const idToken =
   'eyJhbGciOiJSUzI1NiIsImtpZCI6IjdhYThlN2YzOTE2ZGNiM2YyYTUxMWQzY2ZiMTk4YmY0In0.eyJpc3MiOiJodHRwczovL2F1dGgtdGVzdGluZy5pZHVydWd1YXkuZ3ViLnV5L29pZGMvdjEiLCJzdWIiOiI1ODU5IiwiYXVkIjoiODk0MzI5IiwiZXhwIjoxNjAxNTA2Nzc5LCJpYXQiOjE2MDE1MDYxNzksImF1dGhfdGltZSI6MTYwMTUwMTA0OSwiYW1yIjpbInVybjppZHVydWd1YXk6YW06cGFzc3dvcmQiXSwiYWNyIjoidXJuOmlkdXJ1Z3VheTpuaWQ6MSIsImF0X2hhc2giOiJmZ1pFMG1DYml2ZmxBcV95NWRTT09RIn0.r2kRakfFjIXBSWlvAqY-hh9A5Em4n5SWIn9Dr0IkVvnikoAh_E1OPg1o0IT1RW-0qIt0rfkoPUDCCPNrl6d_uNwabsDV0r2LgBSAhjFIQigM37H1buCAn6A5kiUNh8h_zxKxwA8qqia7tql9PUYwNkgslAjgCKR79imMz4j53iw';
 const correctLogoutEndpoint = `https://auth-testing.iduruguay.gub.uy/oidc/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=&state=${mockState}`;
+const correctLogoutProductionEndpoint = `https://auth.iduruguay.gub.uy/oidc/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=&state=${mockState}`;
 const invalidUrl = `https://auth-testing.iduruguay.gub.uy/oidc/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=&state=`;
 
-describe('configuration module and make request type logout integration', () => {
-  it('calls set parameters and makes a logout request which returns non-empty state', async () => {
+describe('configuration & security modules and make request type logout integration', () => {
+  it('calls set parameters and makes a logout request', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
@@ -98,7 +99,194 @@ describe('configuration module and make request type logout integration', () => 
     });
   });
 
-  it('calls set parameters and makes a logout request which returns empty state', async () => {
+  it('calls set parameters and makes a logout request with production set to true', async () => {
+    const production = true;
+    setParameters({ idToken, production });
+
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken,
+      state: '',
+      scope: '',
+    });
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        url: correctLogoutProductionEndpoint,
+      }),
+    );
+    const response = await makeRequest(REQUEST_TYPES.LOGOUT);
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(correctLogoutProductionEndpoint, {
+      method: 'GET',
+      pkPinning: Platform.OS === 'ios',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+    });
+    expect(response).toStrictEqual({
+      state: mockState,
+      message: ERRORS.NO_ERROR,
+      errorCode: ERRORS.NO_ERROR.errorCode,
+      errorDescription: ERRORS.NO_ERROR.errorDescription,
+    });
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+  });
+
+  it('calls set parameters and makes a logout request with empty idTokenHint', async () => {
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+
+    try {
+      await makeRequest(REQUEST_TYPES.LOGOUT);
+    } catch (error) {
+      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+    }
+    expect(fetch).not.toHaveBeenCalled();
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+    expect.assertions(4);
+  });
+
+  it('calls set parameters and makes a logout request with invalid idTokenHint', async () => {
+    setParameters({ idToken: 'invalid_id_token' });
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+
+    try {
+      await makeRequest(REQUEST_TYPES.LOGOUT);
+    } catch (error) {
+      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+    }
+    expect(fetch).not.toHaveBeenCalled();
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+    expect.assertions(4);
+  });
+
+  it('calls set parameters and makes a logout request with invalid production', async () => {
+    setParameters({ production: 'invalid_production' });
+    let parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+
+    try {
+      await makeRequest(REQUEST_TYPES.LOGOUT);
+    } catch (error) {
+      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+    }
+    expect(fetch).not.toHaveBeenCalled();
+
+    parameters = getParameters();
+    expect(parameters).toStrictEqual({
+      redirectUri: '',
+      clientId: '',
+      clientSecret: '',
+      production: false,
+      code: '',
+      accessToken: '',
+      refreshToken: '',
+      tokenType: '',
+      expiresIn: '',
+      idToken: '',
+      state: '',
+      scope: '',
+    });
+    expect.assertions(4);
+  });
+
+  it('calls set parameters and makes a logout request, fetch returns empty state', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
@@ -155,7 +343,9 @@ describe('configuration module and make request type logout integration', () => 
     expect.assertions(5);
   });
 
-  it('calls set parameters with empty idTokenHint and makes a logout request which returns error', async () => {
+  it('calls set parameters and makes a logout request, fetch returns invalid url', async () => {
+    setParameters({ idToken });
+
     let parameters = getParameters();
     expect(parameters).toStrictEqual({
       redirectUri: '',
@@ -167,18 +357,26 @@ describe('configuration module and make request type logout integration', () => 
       refreshToken: '',
       tokenType: '',
       expiresIn: '',
-      idToken: '',
+      idToken,
       state: '',
       scope: '',
     });
-
+    fetch.mockImplementation(() =>
+      Promise.resolve({ status: 200, url: 'InvalidUrl' }),
+    );
     try {
       await makeRequest(REQUEST_TYPES.LOGOUT);
     } catch (error) {
-      expect(error).toBe(ERRORS.INVALID_ID_TOKEN_HINT);
+      expect(error).toBe(ERRORS.INVALID_URL_LOGOUT);
     }
-    expect(fetch).not.toHaveBeenCalled();
-
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(correctLogoutEndpoint, {
+      method: 'GET',
+      pkPinning: Platform.OS === 'ios',
+      sslPinning: {
+        certs: ['certificate'],
+      },
+    });
     parameters = getParameters();
     expect(parameters).toStrictEqual({
       redirectUri: '',
@@ -190,14 +388,14 @@ describe('configuration module and make request type logout integration', () => 
       refreshToken: '',
       tokenType: '',
       expiresIn: '',
-      idToken: '',
-      state: '',
+      idToken,
+      state: mockState,
       scope: '',
     });
-    expect.assertions(4);
+    expect.assertions(5);
   });
 
-  it('calls set parameters, makes a logout request with required parameters and response not OK', async () => {
+  it('calls set parameters and makes a logout request, fetch fails (returning 404)', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
@@ -252,59 +450,7 @@ describe('configuration module and make request type logout integration', () => 
     expect.assertions(5);
   });
 
-  it('calls set parameters, makes a logout request with required parameters and returns invalid url', async () => {
-    setParameters({ idToken });
-
-    let parameters = getParameters();
-    expect(parameters).toStrictEqual({
-      redirectUri: '',
-      clientId: '',
-      clientSecret: '',
-      production: false,
-      code: '',
-      accessToken: '',
-      refreshToken: '',
-      tokenType: '',
-      expiresIn: '',
-      idToken,
-      state: '',
-      scope: '',
-    });
-    fetch.mockImplementation(() =>
-      Promise.resolve({ status: 200, url: Error('Invalid returned url') }),
-    );
-    try {
-      await makeRequest(REQUEST_TYPES.LOGOUT);
-    } catch (error) {
-      expect(error).toBe(ERRORS.INVALID_URL_LOGOUT);
-    }
-    expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith(correctLogoutEndpoint, {
-      method: 'GET',
-      pkPinning: Platform.OS === 'ios',
-      sslPinning: {
-        certs: ['certificate'],
-      },
-    });
-    parameters = getParameters();
-    expect(parameters).toStrictEqual({
-      redirectUri: '',
-      clientId: '',
-      clientSecret: '',
-      production: false,
-      code: '',
-      accessToken: '',
-      refreshToken: '',
-      tokenType: '',
-      expiresIn: '',
-      idToken,
-      state: mockState,
-      scope: '',
-    });
-    expect.assertions(5);
-  });
-
-  it('calls set parameters, makes a logout request with required parameters and fails', async () => {
+  it('calls set parameters and makes a logout request, fetch fails (promise rejected)', async () => {
     setParameters({ idToken });
 
     let parameters = getParameters();
